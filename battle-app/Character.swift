0ef112649c8bef7d20a8f43f62a404335b466d5b
Simon Thomas <simonjthomas@gmail.com>
2015-12-14 00:57:07 +0000

    Before adding any UI elements

---
+++ battle-app/Character.swift
@@ -0,0 +1,74 @@
+//
+//  Character.swift
+//  battle-app
+//
+//  Created by Simon Thomas on 13/12/2015.
+//  Copyright © 2015 Simon Thomas. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp = 100
+    private var _attackPwr = 20
+    private var _name: String
+    private var _player: Int
+    private var _isAlive = true
+    private var _type = "String"
+    private var _startingHP = 100
+    
+    // Getters
+    var hp:Int {
+        return _hp
+    }
+    
+    var attackPwr:Int {
+        return _attackPwr
+    }
+    
+    var name: String {
+        return _name
+    }
+    
+    var player:Int {
+        return _player
+    }
+    
+    var isAlive: Bool {
+        if hp <= 0 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var startingHP:Int {
+        _startingHP = Int(arc4random_uniform(100))
+        return _startingHP
+    }
+    
+    // Initialiser
+    init(_hp:Int,_attackPwr: Int, _name: String, _player: Int, _isAlive: Bool) {
+        self._hp = _hp
+        self._attackPwr = _attackPwr
+        self._name = _name
+        self._player = _player
+        self._isAlive = _isAlive
+        
+    }
+    
+    // Methods
+    func attemptAttack(attacker: Int) -> Bool {
+        /* 
+        Randomly generate attackPwr
+        try attack
+        check if player dies
+        */
+        return true
+    }
+    
+}
---
+++ battle-app/Knight.swift
@@ -0,0 +1,13 @@
+//
+//  Knight.swift
+//  battle-app
+//
+//  Created by Simon Thomas on 14/12/2015.
+//  Copyright © 2015 Simon Thomas. All rights reserved.
+//
+
+import Foundation
+
+class Knight: Character {
+    
+}
